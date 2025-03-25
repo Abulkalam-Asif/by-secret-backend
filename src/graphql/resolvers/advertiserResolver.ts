a7842bfa302c54cc34db473b70b37cc16f354daf
Muhammad Abulkalam Asif <abulkalamasif1129@gmail.com>
2025-03-26 00:38:25 +0500

    registration completed

--- src/graphql/resolvers/advertiserResolver.ts
+++ src/graphql/resolvers/advertiserResolver.ts
@@ -2,6 +2,7 @@ import { Advertiser } from "../../models/Advertiser";
 import transporter from "../../config/nodemailer";
 import * as jose from "jose";
 import cloudinary from "../../config/cloudinary";
+import bcryptjs from "bcryptjs";
 
 export const advertiserResolver = {
   Query: {},
@@ -14,6 +15,18 @@ export const advertiserResolver = {
         // Check if advertiser exists
         const advertiser = await Advertiser.findOne({ email });
         if (advertiser) {
+          // If advertiser exists but has not completed step 3
+          if (!advertiser.password) {
+            advertiser.companyName = companyName;
+            advertiser.fullContactName = fullContactName;
+            await advertiser.save();
+            await sendConfirmationEmail(email);
+            return {
+              success: true,
+              message: "Step 1 completed",
+            };
+          }
+          // If advertiser exists and has completed step 3
           return {
             success: false,
             message: "Advertiser with this email already exists",
@@ -40,8 +53,18 @@ export const advertiserResolver = {
       _: any,
       { password, phone, address, logo, token }: any
     ) => {
+      let payload;
       try {
-        const payload = await verifyToken(token);
+        payload = await verifyToken(token);
+      } catch (error) {
+        console.log("Error verifying token", error);
+        return {
+          success: false,
+          message:
+            "Invalid token. Please reclick the email for verification link",
+        };
+      }
+      try {
         const { email } = payload;
 
         // Check if advertiser exists
@@ -49,12 +72,24 @@ export const advertiserResolver = {
         if (!advertiser) {
           return {
             success: false,
-            message: "Please recheck the email for verification link",
+            message:
+              "Invalid token. Please reclick the email for verification link",
+          };
+        }
+        if (advertiser && advertiser.password) {
+          return {
+            success: false,
+            message: "Advertiser already registered",
           };
         }
 
-        // Upload logo to cloudinary
-        const logoUrl = await uploadLogoToCloudinary(logo);
+        // Upload logo to cloudinary if it exists
+        let logoUrl = "";
+        if (logo) {
+          logoUrl = await uploadLogoToCloudinary(logo);
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 10);
 
         // Update advertiser
         await Advertiser.findOneAndUpdate(
@@ -62,7 +97,7 @@ export const advertiserResolver = {
             email,
           },
           {
-            password,
+            password: hashedPassword,
             phone,
             address,
             logo: logoUrl,
